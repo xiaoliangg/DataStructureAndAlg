6c0aea14911d55dbde9ef60c639253e5b9cee780
xiaoliang <1535523313@qq.com>
2020-07-15 23:54:25 +0800

    添加ArrayList的iterator() ensureCapacity()  trimToSize()的测试方法

--- dataAndAlg/MyDataAndAlg/src/main/java/com/yl/ds/ArrayListTest.java
+++ dataAndAlg/MyDataAndAlg/src/main/java/com/yl/ds/ArrayListTest.java
@@ -3,6 +3,8 @@ package com.yl.ds;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ArrayListTest {
 
@@ -15,5 +17,66 @@ public class ArrayListTest {
         arrayList.get(1);
 
         arrayList.remove("22");
+    }
+
+    /**
+     * 不执行iterator.next()而直接iterator.remove()抛出java.lang.IllegalStateException
+     */
+    @Test
+    public void testIterator(){
+        ArrayList arrayList = new ArrayList(10);
+        arrayList.add("22");
+        arrayList.add("33");
+
+        Iterator iterator = arrayList.iterator();
+        iterator.remove();
+    }
+
+    /**
+     * iterator遍历过程中，集合结构发生改变(add,remove或clear),抛出 java.util.ConcurrentModificationException
+     */
+    @Test
+    public void testIterator2(){
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("22");
+        arrayList.add("33");
+
+        Iterator<String> iterator = arrayList.iterator();
+        while (iterator.hasNext()){
+            arrayList.remove(0);
+            String s = iterator.next(); //抛出 java.util.ConcurrentModificationException
+        }
+    }
+
+    /**
+     * 测试 ensureCapacity()
+     * 设置容量为一个足够大的量以避免数组容量以后的扩展 TODO: 未完成
+     */
+    @Test
+    public void testEnsureCapacity(){
+        ArrayList<String> //ensureCapacity()为ArrayList的特有方法，不是List的方法，所以类型应声明为ArrayList
+                arrayList = new ArrayList<>();
+        arrayList.add("22");
+        arrayList.add("33");
+
+        System.out.println("before ensureCapacity:" + arrayList.size());
+        arrayList.ensureCapacity(2);
+        System.out.println("after ensureCapacity:" + arrayList.size());
+    }
+
+    /**
+     * 测试 trimToSize()
+     * 在所有的ArrayList添加操作完成之后使用以避免浪费空间 TODO: 未完成
+     */
+    @Test
+    public void testTrimToSize(){
+        ArrayList<String> //trimToSize()为ArrayList的特有方法，不是List的方法，所以类型应声明为ArrayList
+                arrayList = new ArrayList<>();
+        arrayList.add("22");
+        arrayList.add("33");
+
+        System.out.println("before trimToSize:" + arrayList.size());
+        arrayList.trimToSize();
+        System.out.println("after trimToSize:" + arrayList.size());
     }
 }
